7ef8297e7da2fe400754241f9a530adf4c0e3556
1 <menathlakvindub@gmail.com>
2023-12-24 21:14:28 +0530

    done | error in heroku server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 
-
 // set the view engines as ejs
 app.set('view engine', 'ejs');
 
@@ -18,11 +17,21 @@ app.get('/(:id)',function(req, res) {
 
 // get sharejs dependencies
 var sharejs = require('share');
-require('redis');
+
+// setup redis server
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+    redisClient = require("redis").createClient();
+}
 
 // options for sharejs
 var options = {
-    db: {type: 'redis'},
+    db: {type: 'redis', client: redisClient},
 };
 
 // attaching the express server to sharejs
